e1156e4624a78b1dc615c66899f50af49e7605fc
Tomasz Kucharski <tomieq@wp.pl>
2021-03-12 10:57:24 +0100

    Implement js class MapObject

---
+++ PublicResources/js/gameMap.js
@@ -0,0 +1,17 @@
+//
+//  GameMap.js
+//  
+//
+//  Created by Tomasz Kucharski on 12/03/2021.
+//
+
+
+class MapObject {
+    constructor(mapX, mapY, imagePath, imageWidth, imageHeight) {
+        this.mapX = mapX;
+        this.mapY = mapY;
+        this.imagePath = imagePath;
+        this.imageWidth = imageWidth;
+        this.imageHeight = imageHeight;
+    }
+}
