0247d0103f554b913a55a0bd3abec0ac67bd3fd5
Tomasz Kucharski <tomieq@wp.pl>
2021-03-14 22:38:23 +0100

    Extended MapPoint to easily move around the map

--- Sources/game/map/MapPoint.swift
+++ Sources/game/map/MapPoint.swift
@@ -12,8 +12,37 @@ struct MapPoint {
     let y: Int
 }
 
+extension MapPoint {
+    func moveRight() -> MapPoint {
+        return MapPoint(x: self.x + 1, y: self.y)
+    }
+    
+    func moveLeft() -> MapPoint {
+        return MapPoint(x: self.x - 1, y: self.y)
+    }
+    
+    func moveUp() -> MapPoint {
+        return MapPoint(x: self.x, y: self.y - 1)
+    }
+    
+    func moveDown() -> MapPoint {
+        return MapPoint(x: self.x, y: self.y + 1)
+    }
+}
+
 extension MapPoint: CustomStringConvertible {
     public var description: String {
         return "[\(self.x),\(self.y)]"
+    }
+}
+
+extension MapPoint: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(x)
+        hasher.combine(y)
+    }
+  
+    static public func ==(lhs: MapPoint, rhs: MapPoint) -> Bool {
+        return lhs.x == rhs.x && lhs.y == rhs.y
     }
 }
