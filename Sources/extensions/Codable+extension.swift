94ac831608b1c6fb7de6a96c2b20f16f7d714d66
Tomasz Kucharski <tomieq@wp.pl>
2021-03-15 17:08:58 +0100

    Implemented Codable extension

---
+++ Sources/extensions/Codable+extension.swift
@@ -0,0 +1,23 @@
+//
+//  Codable+extension.swift
+//  
+//
+//  Created by Tomasz Kucharski on 15/03/2021.
+//
+
+import Foundation
+
+extension Encodable {
+    func toJSONString() -> String? {
+        return try? String(data: JSONEncoder().encode(self), encoding: .utf8)!
+    }
+}
+
+extension Decodable {
+    static func from(JSONString: String) -> Self? {
+        guard let data = JSONString.data(using: .utf8) else {
+            return nil
+        }
+        return try? JSONDecoder().decode(Self.self, from: data)
+    }
+}
