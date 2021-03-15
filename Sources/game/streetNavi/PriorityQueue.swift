eb92f8f037fb88b0d9d2d5e0707ef2799a6b04cc
Tomasz Kucharski <tomieq@wp.pl>
2021-03-15 07:46:58 +0100

    Implement Heap and PriorityQueue

---
+++ Sources/game/streetNavi/PriorityQueue.swift
@@ -0,0 +1,36 @@
+//
+//  PriorityQueue.swift
+//  
+//
+//  Created by Tomasz Kucharski on 14/03/2021.
+//
+
+import Foundation
+
+public struct PriorityQueue<T> {
+    fileprivate var heap: Heap<T>
+    
+    public init(sort: @escaping (T, T) -> Bool) {
+        heap = Heap(priorityFunction: sort)
+    }
+    
+    public var isEmpty : Bool {
+        return heap.isEmpty
+    }
+    
+    public var count : Int {
+        return heap.count
+    }
+    
+    public func peek() -> T? {
+        return heap.peek()
+    }
+    
+    public mutating func enqueue(_ element: T) {
+        heap.enqueue(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        return heap.dequeue()
+    }
+}
