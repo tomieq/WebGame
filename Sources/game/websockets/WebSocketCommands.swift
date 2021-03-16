8f62c3279cf24794126171878c2fc8af1fe84a54
Tomasz Kucharski <tomieq@wp.pl>
2021-03-16 11:59:08 +0100

    Implemented WebSocketCommands.swift

---
+++ Sources/game/websockets/WebSocketCommands.swift
@@ -0,0 +1,37 @@
+//
+//  WebSocketCommands.swift
+//  
+//
+//  Created by Tomasz Kucharski on 15/03/2021.
+//
+
+import Foundation
+
+
+enum WebsocketCommandInType: String, Codable {
+    case tileClicked
+}
+
+class WebsocketInCommand: Codable {
+    var command: WebsocketCommandInType?
+}
+
+class WebsocketInCommandWithPayload<T: Codable>: Codable {
+    var command: WebsocketCommandInType?
+    var payload: T?
+}
+
+
+enum WebsocketCommandOutType: String, Codable {
+    case startVehicle
+}
+
+class WebsocketOutCommand<T: Codable>: Codable {
+
+    var command: WebsocketCommandOutType?
+    let payload: T
+
+    init(_ payload: T) {
+        self.payload = payload
+    }
+}
