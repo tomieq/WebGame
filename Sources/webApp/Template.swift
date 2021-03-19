be6984910ded44a2b9b26484bad6fdb1983e7951
Tomasz Kucharski <tomieq@wp.pl>
2021-03-19 14:03:06 +0100

    Refactored Template by renaming set() func into assign()

--- Sources/webApp/Template.swift
+++ Sources/webApp/Template.swift
@@ -69,7 +69,7 @@ class Template {
         return "{nest-\(name)}"
     }
     
-    func set(variables: [String:String]?, inNest nestName: String) {
+    func assign(variables: [String:String]?, inNest nestName: String) {
         if let nestContent = self.nestedContent[nestName] {
             var content = "\(nestContent)"
             variables?.forEach { variable in
@@ -80,7 +80,7 @@ class Template {
         }
     }
     
-    func set(variables: [String:String]) {
+    func assign(variables: [String:String]) {
         variables.forEach { variable in
             self.content = self.content.replacingOccurrences(of: "{\(variable.key)}", with: variable.value)
         }
