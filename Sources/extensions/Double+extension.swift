fb010c61f41847349bcd01dad1d0360e13eed433
Tomasz Kucharski <tomieq@wp.pl>
2021-03-19 14:00:40 +0100

    Implemented Double extension to format value to money String

--- Sources/extensions/Double+extension.swift
+++ Sources/extensions/Double+extension.swift
@@ -11,4 +11,8 @@ extension Double {
     var string: String {
         return "\(self)"
     }
+
+    var money: String {
+        return "$ \(String(format: "%.0f", self).split(every: 3, backwards: true).joined(separator: " "))"
+    }
 }
