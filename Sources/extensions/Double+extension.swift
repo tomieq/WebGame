c154d2f2cbe6a5169e394d0753691b4c5d211310
Tomasz Kucharski <tomieq@wp.pl>
2021-03-23 09:45:57 +0100

    Implemented Double extension to round the value

--- Sources/extensions/Double+extension.swift
+++ Sources/extensions/Double+extension.swift
@@ -15,4 +15,9 @@ extension Double {
     var money: String {
         return "$ \(String(format: "%.0f", self).split(every: 3, backwards: true).joined(separator: " "))"
     }
+    
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
 }
