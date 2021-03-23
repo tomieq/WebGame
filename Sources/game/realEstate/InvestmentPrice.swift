12671745b39e0fe8fe291a7f0f1afa525268c642
Tomasz Kucharski <tomieq@wp.pl>
2021-03-23 22:37:31 +0100

    Introduced TaxRates and InvestmentPrice list

---
+++ Sources/game/realEstate/InvestmentPrice.swift
@@ -0,0 +1,12 @@
+//
+//  InvestmentPrice.swift
+//  
+//
+//  Created by Tomasz Kucharski on 23/03/2021.
+//
+
+import Foundation
+
+class InvestmentPrice {
+    public static let buildingRoad: Double = 410000
+}
---
+++ Sources/game/realEstate/TaxRates.swift
@@ -0,0 +1,14 @@
+//
+//  TaxRates.swift
+//  
+//
+//  Created by Tomasz Kucharski on 23/03/2021.
+//
+
+import Foundation
+
+class TaxRates {
+    public static let purchaseTax: Double = 8
+    public static let investmentTax: Double = 23
+    public static let incomeTax: Double = 18
+}
