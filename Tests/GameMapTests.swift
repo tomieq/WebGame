ecebf8dbc3949bf467508f390a354bd6f6dc1016
tomieq <tomieq@wp.pl>
2021-10-14 20:32:23 +0200

    Implemented test for adding tiles to map

--- Tests/GameMapTests.swift
+++ Tests/GameMapTests.swift
@@ -18,4 +18,12 @@ final class GameMapTests: XCTestCase {
         XCTAssertEqual(gameMap.height, 21)
         XCTAssertEqual(gameMap.scale, 1)
     }
+    
+    func test_settingTiles() {
+        let gameMap = GameMap(width: 10, height: 10, scale: 1)
+        let tile = GameMapTile(address: MapPoint(x: 3, y: 3), type: .building(size: 4))
+        gameMap.setTiles([tile])
+        XCTAssertEqual(gameMap.tiles.count, 1)
+        XCTAssertEqual(gameMap.tiles.first?.address, tile.address)
+    }
 }
