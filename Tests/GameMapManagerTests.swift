5a4f1f235d5531d938deeaa741160b634d9814b9
tomieq <tomieq@wp.pl>
2021-10-15 11:31:38 +0200

    Added tests in GameMapManagerTests

--- Tests/GameMapManagerTests.swift
+++ Tests/GameMapManagerTests.swift
@@ -24,6 +24,13 @@ final class GameMapManagerTests: XCTestCase {
         let mapManager = GameMapManager(map)
         let mapContent = "s,s"
         mapManager.loadMapFrom(content: mapContent)
+        /* it should be placed @(0,0) (1,0)
+         --------
+        ⎹ •  x → ⎸
+        ⎹ y      ⎸
+        ⎹ ↓      ⎸
+         --------
+         */
         XCTAssertEqual(map.tiles.count, 2)
         XCTAssertNotNil(map.getTile(address:MapPoint(x: 0, y: 0)))
         XCTAssertNotNil(map.getTile(address:MapPoint(x: 1, y: 0)))
@@ -34,6 +41,13 @@ final class GameMapManagerTests: XCTestCase {
         let mapManager = GameMapManager(map)
         let mapContent = "s\ns"
         mapManager.loadMapFrom(content: mapContent)
+        /* it should be placed @(0,0) (0,1)
+         --------
+        ⎹ •  x → ⎸
+        ⎹ y      ⎸
+        ⎹ ↓      ⎸
+         --------
+         */
         XCTAssertEqual(map.tiles.count, 2)
         XCTAssertNotNil(map.getTile(address:MapPoint(x: 0, y: 0)))
         XCTAssertNotNil(map.getTile(address:MapPoint(x: 0, y: 1)))
@@ -47,6 +61,7 @@ final class GameMapManagerTests: XCTestCase {
         XCTAssertEqual(map.tiles.count, 2)
         XCTAssertNotNil(map.getTile(address:MapPoint(x: 0, y: 0)))
         XCTAssertNotNil(map.getTile(address:MapPoint(x: 1, y: 0)))
+        XCTAssertNil(map.getTile(address:MapPoint(x: 2, y: 0)))
     }
     
     func test_loadingMapFromString_exceedingMapHeigh() {
@@ -63,5 +78,70 @@ final class GameMapManagerTests: XCTestCase {
         let mapContent = "s,s,s,s,s\ns,s,s,s,s\ns,s,s,s,s\ns,s,s,s,s"
         mapManager.loadMapFrom(content: mapContent)
         XCTAssertEqual(map.tiles.count, 9)
+    }
+    
+    func test_localStreetCurveBottomEvaluation() {
+        let map = GameMap(width: 3, height: 3, scale: 0.2)
+        let mapManager = GameMapManager(map)
+        let mapContent = "s,s,\n ,s"
+        mapManager.loadMapFrom(content: mapContent)
+        /*
+         --------
+        ⎹ •  x → ⎸
+        ⎹ y      ⎸
+        ⎹ ↓      ⎸
+         --------
+         
+         (0,0)→  (1,0)↴
+                 (1,1)↓
+         */
+        XCTAssertEqual(map.tiles.count, 3)
+        XCTAssertEqual(map.getTile(address: MapPoint(x: 0, y: 0))?.type, TileType.street(type: .local(.localDeadEndX1)))
+        XCTAssertEqual(map.getTile(address: MapPoint(x: 1, y: 0))?.type, TileType.street(type: .local(.localCurveBottom)))
+        XCTAssertEqual(map.getTile(address: MapPoint(x: 1, y: 1))?.type, TileType.street(type: .local(.localDeadEndY2)))
+    }
+
+    func test_localStreetCurveLeftEvaluation() {
+        let map = GameMap(width: 3, height: 3, scale: 0.2)
+        let mapManager = GameMapManager(map)
+        let mapContent = " ,s,\ns,s"
+        mapManager.loadMapFrom(content: mapContent)
+        /*
+         --------
+        ⎹ •  x → ⎸
+        ⎹ y      ⎸
+        ⎹ ↓      ⎸
+         --------
+         
+                 (1,0)↓
+          (0,1)← (1,1)↲
+         */
+        XCTAssertEqual(map.tiles.count, 3)
+        XCTAssertEqual(map.getTile(address: MapPoint(x: 1, y: 0))?.type, TileType.street(type: .local(.localDeadEndY1)))
+        XCTAssertEqual(map.getTile(address: MapPoint(x: 1, y: 1))?.type, TileType.street(type: .local(.localCurveLeft)))
+        XCTAssertEqual(map.getTile(address: MapPoint(x: 0, y: 1))?.type, TileType.street(type: .local(.localDeadEndX1)))
+    }
+    
+    func test_addLocalStreetEvaluation() {
+        let map = GameMap(width: 3, height: 3, scale: 0.2)
+        let mapManager = GameMapManager(map)
+        let mapContent = " ,s,\ns,s"
+        mapManager.loadMapFrom(content: mapContent)
+        /*
+         --------
+        ⎹ •  x → ⎸
+        ⎹ y      ⎸
+        ⎹ ↓      ⎸
+         --------
+         
+                 (1,0)↓
+          (0,1)← (1,1)↲
+         */
+        XCTAssertEqual(map.tiles.count, 3)
+        XCTAssertEqual(map.getTile(address: MapPoint(x: 1, y: 1))?.type, TileType.street(type: .local(.localCurveLeft)))
+        mapManager.addStreet(address: MapPoint(x: 2, y: 1))
+        XCTAssertEqual(map.tiles.count, 4)
+        XCTAssertEqual(map.getTile(address: MapPoint(x: 1, y: 1))?.type, TileType.street(type: .local(.localXIntersection1)))
+        XCTAssertEqual(map.getTile(address: MapPoint(x: 2, y: 1))?.type, TileType.street(type: .local(.localDeadEndX2)))
     }
 }
