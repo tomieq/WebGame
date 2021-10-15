d3b6d51baf08f6ebd34d1ad653a4998e3342b2ca
tomieq <tomieq@wp.pl>
2021-10-15 12:51:18 +0200

    Implemented tests for StreetNavi routePoints

--- Tests/StreetNaviTests.swift
+++ Tests/StreetNaviTests.swift
@@ -99,4 +99,128 @@ final class StreetNaviTests: XCTestCase {
         let navi = StreetNavi(gameMap: map)
         XCTAssertEqual(navi.findNearestStreetPoint(for: MapPoint(x: 0, y: 0)), MapPoint(x: 1, y: 1))
     }
+    
+    func test_routePoints_startNotOnStreet() {
+        let map = GameMap(width: 10, height: 10, scale: 0.4)
+        let mapManager = GameMapManager(map)
+        let mapContent = " ,s,s,s,s,s,s"
+        mapManager.loadMapFrom(content: mapContent)
+        let navi = StreetNavi(gameMap: map)
+        let routePoints = navi.routePoints(from: MapPoint(x: 0, y: 0), to: MapPoint(x: 5, y: 0))
+        XCTAssertNil(routePoints)
+    }
+    
+    func test_routePoints_straightHorizontalLine() {
+        let map = GameMap(width: 10, height: 10, scale: 0.4)
+        let mapManager = GameMapManager(map)
+        let mapContent = "s,s,s,s,s,s"
+        mapManager.loadMapFrom(content: mapContent)
+        let navi = StreetNavi(gameMap: map)
+        let routePoints = navi.routePoints(from: MapPoint(x: 0, y: 0), to: MapPoint(x: 5, y: 0))
+        XCTAssertNotNil(routePoints)
+        // this route should contain only 2 points
+        XCTAssertEqual(routePoints?.count, 2)
+        XCTAssertEqual(routePoints?.first, MapPoint(x: 0, y: 0))
+        XCTAssertEqual(routePoints?.last, MapPoint(x: 5, y: 0))
+    }
+    
+    func test_routePoints_straightVerticalLine() {
+        let map = GameMap(width: 10, height: 10, scale: 0.4)
+        let mapManager = GameMapManager(map)
+        let mapContent = """
+                            s
+                            s
+                            s
+                            s
+                            s
+                            s
+"""
+        mapManager.loadMapFrom(content: mapContent)
+        let navi = StreetNavi(gameMap: map)
+        let routePoints = navi.routePoints(from: MapPoint(x: 0, y: 0), to: MapPoint(x: 0, y: 5))
+        XCTAssertNotNil(routePoints)
+        // this route should contain only 2 points
+        XCTAssertEqual(routePoints?.count, 2)
+        XCTAssertEqual(routePoints?.first, MapPoint(x: 0, y: 0))
+        XCTAssertEqual(routePoints?.last, MapPoint(x: 0, y: 5))
+    }
+    
+    func test_routePoints_oneBend() {
+        let map = GameMap(width: 10, height: 10, scale: 0.4)
+        let mapManager = GameMapManager(map)
+        let mapContent = """
+                            s,s,s,s,s,s
+                            s
+                            s
+                            s
+                            s
+                            s
+"""
+        mapManager.loadMapFrom(content: mapContent)
+        let navi = StreetNavi(gameMap: map)
+        let routePoints = navi.routePoints(from: MapPoint(x: 0, y: 5), to: MapPoint(x: 5, y: 0))
+        XCTAssertNotNil(routePoints)
+        // this route should contain 3 points
+        XCTAssertEqual(routePoints?.count, 3)
+        XCTAssertEqual(routePoints?[0], MapPoint(x: 0, y: 5))
+        XCTAssertEqual(routePoints?[1], MapPoint(x: 0, y: 0))
+        XCTAssertEqual(routePoints?[2], MapPoint(x: 5, y: 0))
+    }
+    
+    func test_routePoints_twoBends() {
+        let map = GameMap(width: 10, height: 10, scale: 0.4)
+        let mapManager = GameMapManager(map)
+        let mapContent = """
+                            s,s,s,s,s
+                            s, , , ,s
+                            s, , , ,s
+                            s, , , ,s
+                            s, , , ,s
+                            s, , , ,s
+"""
+        mapManager.loadMapFrom(content: mapContent)
+        let navi = StreetNavi(gameMap: map)
+        let routePoints = navi.routePoints(from: MapPoint(x: 0, y: 5), to: MapPoint(x: 4, y: 5))
+        XCTAssertNotNil(routePoints)
+        // this route should contain 3 points
+        XCTAssertEqual(routePoints?.count, 4)
+        XCTAssertEqual(routePoints?[0], MapPoint(x: 0, y: 5))
+        XCTAssertEqual(routePoints?[1], MapPoint(x: 0, y: 0))
+        XCTAssertEqual(routePoints?[2], MapPoint(x: 4, y: 0))
+        XCTAssertEqual(routePoints?[3], MapPoint(x: 4, y: 5))
+    }
+    
+    
+    func test_routePoints_reloadMapShorterWay() {
+        let map = GameMap(width: 10, height: 10, scale: 0.4)
+        let mapManager = GameMapManager(map)
+        let mapContent = """
+                            s,s,s,s,s,s
+                            s, ,s
+                            s, ,s
+                            s, ,s
+                            s, ,s
+                            s, ,s
+"""
+        mapManager.loadMapFrom(content: mapContent)
+        let navi = StreetNavi(gameMap: map)
+        var routePoints = navi.routePoints(from: MapPoint(x: 0, y: 5), to: MapPoint(x: 2, y: 5))
+        XCTAssertNotNil(routePoints)
+        // this route should contain 3 points
+        XCTAssertEqual(routePoints?.count, 4)
+        XCTAssertEqual(routePoints?[0], MapPoint(x: 0, y: 5))
+        XCTAssertEqual(routePoints?[1], MapPoint(x: 0, y: 0))
+        XCTAssertEqual(routePoints?[2], MapPoint(x: 2, y: 0))
+        XCTAssertEqual(routePoints?[3], MapPoint(x: 2, y: 5))
+        
+        mapManager.addStreet(address: MapPoint(x: 1, y: 3))
+        navi.reload()
+        // now navi should return shorter way
+        routePoints = navi.routePoints(from: MapPoint(x: 0, y: 5), to: MapPoint(x: 2, y: 5))
+        XCTAssertEqual(routePoints?.count, 4)
+        XCTAssertEqual(routePoints?[0], MapPoint(x: 0, y: 5))
+        XCTAssertEqual(routePoints?[1], MapPoint(x: 0, y: 3))
+        XCTAssertEqual(routePoints?[2], MapPoint(x: 2, y: 3))
+        XCTAssertEqual(routePoints?[3], MapPoint(x: 2, y: 5))
+    }
 }
