76a12026b111df95df12381e1bfb6114fadcd3ea
tomieq <tomieq@wp.pl>
2021-10-19 11:55:53 +0200

    Added test cases for corner cases with road investment

--- Tests/ConstructionServicesTests.swift
+++ Tests/ConstructionServicesTests.swift
@@ -121,7 +121,7 @@ final class ConstructionServicesTests: XCTestCase {
         XCTAssertEqual(offer.invoice.tax, 10000)
     }
     
-    func test_startRoadInvestment() {
+    func test_startRoadInvestment_addressNotFound() {
         let map = GameMap(width: 2, height: 2, scale: 0.2)
         let mapManager = GameMapManager(map)
         
@@ -130,12 +130,50 @@ final class ConstructionServicesTests: XCTestCase {
         let dataStore = DataStoreMemoryProvider()
         let taxRates = TaxRates()
         let centralBank = CentralBank(dataStore: dataStore, taxRates: taxRates)
-        centralBank.taxRates.investmentTax = 0.5
         
         let constructionServices = ConstructionServices(mapManager: mapManager, centralBank: centralBank, time: time)
         XCTAssertThrowsError(try constructionServices.startRoadInvestment(address: MapPoint(x: 0, y: 0), playerUUID: "tester")){ error in
             XCTAssertEqual(error as! ConstructionServicesError, .addressNotFound)
         }
+    }
+    
+    func test_startRoadInvestment_playerIsNotTheOwner() {
+        let map = GameMap(width: 2, height: 2, scale: 0.2)
+        let mapManager = GameMapManager(map)
         
+        let time = GameTime()
+        
+        let dataStore = DataStoreMemoryProvider()
+        let taxRates = TaxRates()
+        let centralBank = CentralBank(dataStore: dataStore, taxRates: taxRates)
+        
+        let land = Land(address: MapPoint(x: 0, y: 0))
+        dataStore.create(land)
+        
+        let constructionServices = ConstructionServices(mapManager: mapManager, centralBank: centralBank, time: time)
+        XCTAssertThrowsError(try constructionServices.startRoadInvestment(address: MapPoint(x: 0, y: 0), playerUUID: "tester")){ error in
+            XCTAssertEqual(error as! ConstructionServicesError, .playerIsNotPropertyOwner)
+        }
+    }
+    
+    func test_startRoadInvestment_noAccessToRoad() {
+        let map = GameMap(width: 2, height: 2, scale: 0.2)
+        let mapManager = GameMapManager(map)
+        
+        let time = GameTime()
+        
+        let dataStore = DataStoreMemoryProvider()
+        let taxRates = TaxRates()
+        let centralBank = CentralBank(dataStore: dataStore, taxRates: taxRates)
+        
+        let land = Land(address: MapPoint(x: 0, y: 0), ownerUUID: "tester")
+        dataStore.create(land)
+        
+        let constructionServices = ConstructionServices(mapManager: mapManager, centralBank: centralBank, time: time)
+        XCTAssertThrowsError(try constructionServices.startRoadInvestment(address: MapPoint(x: 0, y: 0), playerUUID: "tester")){ error in
+            XCTAssertEqual(error as! ConstructionServicesError, .noDirectAccessToRoad)
+        }
+    }
+    
     }
 }
