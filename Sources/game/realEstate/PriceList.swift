a267dbc6017497633cdee4598131ef381402905a
tomieq <tomieq@wp.pl>
2021-10-19 23:18:21 +0200

    Adjusted prices in PriceList

--- Sources/game/realEstate/PriceList.swift
+++ Sources/game/realEstate/PriceList.swift
@@ -18,13 +18,13 @@ class PriceList {
     public var propertyValueDistanceFromRoadLoss: Double = 0.6
     public var propertyValueAntennaSurroundingLoss: Double = 0.22
     // property value gain
-    public var propertyValueDistanceFromResidentialBuildingGain = 0.4
+    public var propertyValueDistanceFromResidentialBuildingGain = 0.2
     // montly costs
     public var montlyResidentialBuildingCost = 1300.0
     public var montlyResidentialBuildingCostPerStorey = 1100.0
     public var monthlyResidentialBuildingOwnerIncomePerFlat: Double = 300.0
     public var monthlyBillsForRentedApartment: Double = 452.0
     public var monthlyBillsForUnrentedApartment: Double = 180.0
-    public var monthlyApartmentRentalFee: Double = 2330
+    public var monthlyApartmentRentalFee: Double = 2300
     public var monthlyApartmentBuildingOwnerFee: Double = 930
 }
