30dba956efa80b5f4b16ea093316c426b165919f
tomieq <tomieq@wp.pl>
2021-10-20 14:20:52 +0200

    Simplify ClickTileRouterTests

--- Tests/ClickTileRouterTests.swift
+++ Tests/ClickTileRouterTests.swift
@@ -25,37 +25,27 @@ final class ClickTileRouterTests: XCTestCase {
     
     func test_openRoadInfoSomebodysRoad() {
         let dataStore = DataStoreMemoryProvider()
-        let taxRates = TaxRates()
-        let centralBank = CentralBank(dataStore: dataStore, taxRates: taxRates)
-        let map = GameMap(width: 200, height: 200, scale: 0.2)
+        let map = GameMap(width: 2, height: 2, scale: 0.2)
         let mapManager = GameMapManager(map)
-        let agent = RealEstateAgent(mapManager: mapManager, centralBank: centralBank, delegate: nil)
+        mapManager.loadMapFrom(content: "s,s")
         
-        let road1 = Road(land: Land(address: MapPoint(x: 20, y: 20), ownerUUID: "person1"))
-        let road2 = Road(land: Land(address: MapPoint(x: 21, y: 20)))
-        dataStore.create(road1)
-        dataStore.create(road2)
+        let road = Road(land: Land(address: MapPoint(x: 0, y: 0), ownerUUID: "person1"))
+        dataStore.create(road)
         
-        agent.syncMapWithDataStore()
         let router = ClickTileRouter(map: map, dataStore: dataStore)
-        XCTAssertEqual(router.action(address: MapPoint(x: 20, y: 20), playerUUID: "anybody"), .roadInfo)
+        XCTAssertEqual(router.action(address: MapPoint(x: 0, y: 0), playerUUID: "anybody"), .roadInfo)
     }
     
     func test_openRoadManager() {
         let dataStore = DataStoreMemoryProvider()
-        let taxRates = TaxRates()
-        let centralBank = CentralBank(dataStore: dataStore, taxRates: taxRates)
-        let map = GameMap(width: 200, height: 200, scale: 0.2)
+        let map = GameMap(width: 2, height: 2, scale: 0.2)
         let mapManager = GameMapManager(map)
-        let agent = RealEstateAgent(mapManager: mapManager, centralBank: centralBank, delegate: nil)
+        mapManager.loadMapFrom(content: "s,s")
         
-        let road1 = Road(land: Land(address: MapPoint(x: 20, y: 20), ownerUUID: "person1"))
-        let road2 = Road(land: Land(address: MapPoint(x: 21, y: 20)))
-        dataStore.create(road1)
-        dataStore.create(road2)
+        let road = Road(land: Land(address: MapPoint(x: 0, y: 0), ownerUUID: "person1"))
+        dataStore.create(road)
         
-        agent.syncMapWithDataStore()
         let router = ClickTileRouter(map: map, dataStore: dataStore)
-        XCTAssertEqual(router.action(address: MapPoint(x: 20, y: 20), playerUUID: "person1"), .roadManager)
+        XCTAssertEqual(router.action(address: MapPoint(x: 0, y: 0), playerUUID: "person1"), .roadManager)
     }
 }
