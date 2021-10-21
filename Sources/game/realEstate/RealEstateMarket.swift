5322cb78ee93820907cb8a22d183bf208c718baa
tomieq <tomieq@wp.pl>
2021-10-21 13:31:57 +0200

    Implemented checking if property exists

--- Sources/game/realEstate/RealEstateMarket.swift
+++ Sources/game/realEstate/RealEstateMarket.swift
@@ -13,13 +13,34 @@ enum RealEstateMarketError: Error {
 
 class RealEstateMarket {
     private let gameMap: GameMap
+    private let dataStore: DataStoreProvider
     
-    init(gameMap: GameMap) {
+    init(gameMap: GameMap, dataStore: DataStoreProvider) {
         self.gameMap = gameMap
+        self.dataStore = dataStore
     }
     
     func createOffer(address: MapPoint, netValue: Double) throws {
         guard self.gameMap.isAddressOnMap(address) else {
+            throw RealEstateMarketError.propertyDoesNotExist
+        }
+        guard let propertyType = self.gameMap.getTile(address: address)?.propertyType else {
+            throw RealEstateMarketError.propertyDoesNotExist
+        }
+        let property: Property?
+        switch propertyType {
+            
+        case .land:
+            let land: Land? = self.dataStore.find(address: address)
+            property = land
+        case .road:
+            let road: Road? = self.dataStore.find(address: address)
+            property = road
+        case .residentialBuilding:
+            let building: ResidentialBuilding? = self.dataStore.find(address: address)
+            property = building
+        }
+        guard let property = property else {
             throw RealEstateMarketError.propertyDoesNotExist
         }
     }
--- Tests/RealEstateMarketTests.swift
+++ Tests/RealEstateMarketTests.swift
@@ -14,10 +14,21 @@ final class RealEstateMarketTests: XCTestCase {
     
     func test_createOfferOutsideMap() {
         
+        let dataStore = DataStoreMemoryProvider()
         let map = GameMap(width: 10, height: 10, scale: 0.5)
-        let market = RealEstateMarket(gameMap: map)
+        let market = RealEstateMarket(gameMap: map, dataStore: dataStore)
         
         XCTAssertThrowsError(try market.createOffer(address: MapPoint(x: 30, y: 30), netValue: 3000)){ error in
+            XCTAssertEqual(error as? RealEstateMarketError, .propertyDoesNotExist)
+        }
+    }
+    
+    func test_createOfferOnNonExistingProperty() {
+        let dataStore = DataStoreMemoryProvider()
+        let map = GameMap(width: 10, height: 10, scale: 0.5)
+        let market = RealEstateMarket(gameMap: map, dataStore: dataStore)
+        
+        XCTAssertThrowsError(try market.createOffer(address: MapPoint(x: 5, y: 5), netValue: 3000)){ error in
             XCTAssertEqual(error as? RealEstateMarketError, .propertyDoesNotExist)
         }
     }
