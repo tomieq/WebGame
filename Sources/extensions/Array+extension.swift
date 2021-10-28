5f24f7ef72a15d826a0180955bb606b13dd0418b
tomieq <tomieq@wp.pl>
2021-10-28 13:25:27 +0200

    Added array unique extension

--- Sources/extensions/Array+extension.swift
+++ Sources/extensions/Array+extension.swift
@@ -51,3 +51,14 @@ extension Array {
     }
     
 }
+
+extension Array where Element: Equatable {
+    var unique: [Element] {
+        var uniqueValues: [Element] = []
+        forEach { item in
+            guard !uniqueValues.contains(item) else { return }
+            uniqueValues.append(item)
+        }
+        return uniqueValues
+    }
+}
