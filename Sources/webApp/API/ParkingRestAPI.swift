936c6e82ad79a5cfd327dbcf865a317d8d5bd310
tomieq <tomieq@wp.pl>
2021-11-05 13:46:51 +0100

    Implemented parkingBalance.html

--- Sources/webApp/API/ParkingRestAPI.swift
+++ Sources/webApp/API/ParkingRestAPI.swift
@@ -114,7 +114,31 @@ class ParkingRestAPI: RestAPI {
             view.setInitialContent(html: balanceView.output())
             
             
-            return .ok(.html(view.output()))
+            return view.output().asResponse
+        }
+        
+        // MARK: parkingBalance.html
+        server.GET["/parkingBalance.html"] = { request, _ in
+            request.disableKeepAlive = true
+            guard let playerSessionID = request.queryParam("playerSessionID"),
+                let session = PlayerSessionManager.shared.getPlayerSession(playerSessionID: playerSessionID) else {
+                    return self.htmlError("Invalid request! Missing session ID.")
+            }
+            guard let address = request.mapPoint else {
+                return self.htmlError("Invalid request! Missing address.")
+            }
+            guard let parking: Parking = self.dataStore.find(address: address) else {
+                return self.htmlError("Property at \(address.description) not found!")
+            }
+            let ownerID = parking.ownerUUID
+            guard session.playerUUID == ownerID else {
+                return self.htmlError("Property at \(address.description) is not yours!")
+            }
+            let balanceView = PropertyBalanceView()
+            balanceView.setMonthlyCosts(self.gameEngine.propertyBalanceCalculator.getMontlyCosts(address: address))
+            balanceView.setMonthlyIncome(self.gameEngine.propertyBalanceCalculator.getMonthlyIncome(address: address))
+            balanceView.setProperty(parking)
+            return balanceView.output().asResponse
         }
     }
 
