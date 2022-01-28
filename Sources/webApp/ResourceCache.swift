438af7a6de65cbd90c4d09b9b77684cddf286729
tomieq <tomieq@wp.pl>
2022-01-28 09:48:48 +0100

    Added possibility to disable cache

--- Sources/webApp/ResourceCache.swift
+++ Sources/webApp/ResourceCache.swift
@@ -11,12 +11,14 @@ class ResourceCache {
     public static let shared = ResourceCache()
     
     private var cache: [String:String]
+    private let caching = false
     
     private init() {
         self.cache = [:]
     }
     
     func getAppResource(_ relativePath: String) -> String {
+        guard self.caching else { return Resource.getAppResource(relativePath: relativePath) }
         if let content = self.cache[relativePath] {
             return content
         }
