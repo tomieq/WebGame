b6d77a067fd9c4e1c9c1c2d4d012aa28b9b5caad
tomieq <tomieq@wp.pl>
2022-02-14 10:27:26 +0100

    Store gameMap in AddonsMap

--- Sources/game/map/addons/AddonsMap.swift
+++ Sources/game/map/addons/AddonsMap.swift
@@ -16,7 +16,12 @@ struct AddonMapTile {
 
 class AddonsMap {
     private var addonTiles: [MapPoint:AddonMapTile] = [:]
+    private var gameMap: GameMap
     var tiles: [AddonMapTile] {
         return Array(self.addonTiles.values)
+    }
+    
+    init(gameMap: GameMap) {
+        self.gameMap = gameMap
     }
 }
