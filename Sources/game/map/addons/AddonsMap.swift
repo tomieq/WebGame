e616cc80e4d3c7eb71221ba92a1397af18122f0d
tomieq <tomieq@wp.pl>
2022-02-14 10:41:19 +0100

    Implemented adding cars on parking in addons layer

--- Sources/game/map/addons/AddonsMap.swift
+++ Sources/game/map/addons/AddonsMap.swift
@@ -23,5 +23,35 @@ class AddonsMap {
     
     init(gameMap: GameMap) {
         self.gameMap = gameMap
+        
+        for tile in self.gameMap.tiles {
+            let address = tile.address
+            switch tile.type {
+            case .parking(let parkingType):
+                self.addonTiles[address] = AddonMapTile(address: address, type: .carsOnParking(direction: parkingType.direction, size: 10))
+            default:
+                break
+            }
+        }
+    }
+}
+
+fileprivate extension ParkingType {
+    var direction: CarsOnParkingDirection {
+        switch self {
+            
+        case .bottomConnection:
+            return .Y
+        case .topConnection:
+            return .Y
+        case .leftConnection:
+            return .X
+        case .rightConnection:
+            return .X
+        case .X:
+            return .X
+        case .Y:
+            return .Y
+        }
     }
 }
