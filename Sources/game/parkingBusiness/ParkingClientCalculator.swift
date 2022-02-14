e72b9f99c170fd1127d47a3366ff55bc249a3a8c
tomieq <tomieq@wp.pl>
2022-02-14 11:42:52 +0100

    Implemented ParkingClientCalculator

---
+++ Sources/game/parkingBusiness/ParkingClientCalculator.swift
@@ -0,0 +1,78 @@
+//
+//  ParkingClientCalculator.swift
+//  
+//
+//  Created by Tomasz Kucharski on 14/02/2022.
+//
+
+import Foundation
+
+class ParkingClientCalculator {
+    
+    let mapManager: GameMapManager
+    let dataStore: DataStoreProvider
+
+    init(mapManager: GameMapManager, dataStore: DataStoreProvider) {
+        self.mapManager = mapManager
+        self.dataStore = dataStore
+    }
+    
+    
+    func calculateCarsForParking(address: MapPoint) -> Double {
+        
+        let parking: Parking? = self.dataStore.find(address: address)
+        let carsInTheArea = self.getCarsAroundAddress(address)
+        let competitorAddresses = self.getParkingsAroundAddress(address)
+        let competitors: [Parking] = competitorAddresses.compactMap{ self.dataStore.find(address: $0) }
+        // competitorTrusts stores a map of competitor's shares for address (competitors' parking trusts)
+        var competitorTrusts: [MapPoint: [Double]] = [:]
+        
+        for competitorAddress in competitorAddresses {
+            let carsInCompetitorRange = self.getCarsAroundAddress(competitorAddress)
+            let competitorTrust = competitors.first{ $0.address == competitorAddress }?.trustLevel ?? 1.0
+            for address in carsInCompetitorRange.keys {
+                competitorTrusts[address, default: []].append(competitorTrust)
+            }
+        }
+        let myTrust = parking?.trustLevel ?? 1.0
+        var amountOfCars: Double = 0
+        for (address, numberOfCars) in carsInTheArea {
+            if let trusts = competitorTrusts[address] {
+                let sumOfTrust = myTrust + trusts.reduce(0, +)
+                amountOfCars += numberOfCars * (myTrust / sumOfTrust)
+            } else {
+                amountOfCars += numberOfCars
+            }
+        }
+
+        return amountOfCars
+    }
+
+    private func getCarsAroundAddress(_ address: MapPoint) -> [MapPoint: Double] {
+        var carsPerAddress: [MapPoint: Double] = [:]
+        for radius in (1...2) {
+            for neighbour in self.mapManager.map.getNeighbourAddresses(to: address, radius: radius) {
+                if let tileType = self.mapManager.map.getTile(address: neighbour)?.type {
+                    let carsOnProperty = tileType.carsOnProperty
+                    if carsOnProperty > 0 {
+                        carsPerAddress[neighbour] = tileType.carsOnProperty
+                    }
+                }
+            }
+        }
+        return carsPerAddress
+    }
+    
+    
+    func getParkingsAroundAddress(_ address: MapPoint) -> [MapPoint] {
+        var parkings: [MapPoint] = []
+        for radius in (1...4) {
+            for parking in self.mapManager.map.getNeighbourAddresses(to: address, radius: radius) {
+                if self.mapManager.map.getTile(address: parking)?.isParking() ?? false {
+                    parkings.append(parking)
+                }
+            }
+        }
+        return parkings
+    }
+}
---
+++ Tests/ParkingClientCalculatorTests.swift
@@ -0,0 +1,124 @@
+//
+//  ParkingClientCalculatorTests.swift
+//  
+//
+//  Created by Tomasz Kucharski on 14/02/2022.
+//
+
+import Foundation
+import XCTest
+@testable import WebGameLib
+
+class ParkingClientCalculatorTests: XCTestCase {
+
+    func test_amountOfCarsNearbyResidentialBuilding() {
+        let calculator = self.makeCalculator()
+        calculator.mapManager.loadMapFrom(content: "b")
+        XCTAssertEqual(calculator.calculateCarsForParking(address: MapPoint(x: 1, y: 1)), 4)
+    }
+    
+    func test_amountOfCarsTwoTilesAwayFromResidentialBuilding() {
+        let calculator = self.makeCalculator()
+        calculator.mapManager.loadMapFrom(content: "b,R")
+        XCTAssertEqual(calculator.calculateCarsForParking(address: MapPoint(x: 0, y: 2)), 4 + 10)
+    }
+    
+    func test_amountOfCarsNearbyHospital() {
+        let calculator = self.makeCalculator()
+        calculator.mapManager.loadMapFrom(content: "-,H")
+        XCTAssertEqual(calculator.calculateCarsForParking(address: MapPoint(x: 1, y: 1)), 12)
+    }
+    
+    func test_amountOfCarsNearSchool() {
+        let calculator = self.makeCalculator()
+        calculator.mapManager.loadMapFrom(content: "-,Q")
+        XCTAssertEqual(calculator.calculateCarsForParking(address: MapPoint(x: 1, y: 1)), 5)
+    }
+    
+    func test_amountOfparkingsInTheArea() {
+        let calculator = self.makeCalculator()
+        calculator.mapManager.loadMapFrom(content: "s,s,s\nc,-,c")
+        XCTAssertEqual(calculator.getParkingsAroundAddress(MapPoint(x: 1, y: 1)).count, 2)
+    }
+    
+    func test_amountOfCars_twoParkingsOneBuilding() {
+        let calculator = self.makeCalculator()
+        let layout = """
+                    R,c,c
+                    s,s,s,s
+                    """
+        calculator.mapManager.loadMapFrom(content: layout)
+        XCTAssertEqual(calculator.calculateCarsForParking(address: MapPoint(x: 2, y: 0)), 10 / 2)
+    }
+    
+    func test_amountOfCars_threeParkingsOneBuilding() {
+        let calculator = self.makeCalculator()
+        let layout = """
+                    s,s,s,s,s,s,s,s,s
+                    -,c,-,B,-,c
+                    -,-,-,c,
+                    """
+        calculator.mapManager.loadMapFrom(content: layout)
+        XCTAssertEqual(calculator.calculateCarsForParking(address: MapPoint(x: 3, y: 2)), 6 / 3)
+    }
+    
+    func test_amountOfCars_threeParkingsTwoBuildings() {
+        let calculator = self.makeCalculator()
+        let layout = """
+                    s,s,s,s,s,s,s,s,s
+                    -,c,R,B,-,c
+                    -,-,-,c,
+                    """
+        calculator.mapManager.loadMapFrom(content: layout)
+        XCTAssertEqual(calculator.calculateCarsForParking(address: MapPoint(x: 3, y: 2)), 6 / 3 + 10 / 2)
+    }
+    
+    func test_amountOfCarsTwoParkings_lowerTrust() {
+        let calculator = self.makeCalculator()
+        let dataStore = calculator.dataStore
+        let layout = """
+                    s,s,s,s,s,s,s,s,s
+                    -,c,B,c
+                    """
+        calculator.mapManager.loadMapFrom(content: layout)
+        let address1 = MapPoint(x: 1, y: 1)
+        let address2 = MapPoint(x: 3, y: 1)
+        
+        let uuid = dataStore.create(Parking(land: Land(address: address1)))
+        dataStore.create(Parking(land: Land(address: address2), trustLevel: 0.5))
+        
+        XCTAssertEqual(calculator.calculateCarsForParking(address: address1), 4)
+        XCTAssertEqual(calculator.calculateCarsForParking(address: address2), 2)
+        
+        dataStore.update(ParkingMutation(uuid: uuid, attributes: [.trustLevel(0.5)]))
+        XCTAssertEqual(calculator.calculateCarsForParking(address: address1), 3)
+    }
+    
+    func test_amountOfCarsThreeParkings_lowerTrust() {
+        let calculator = self.makeCalculator()
+        let dataStore = calculator.dataStore
+        let layout = """
+                    s,s,s,s,s,s,s,s,s
+                    -,c,B,c,c
+                    """
+        calculator.mapManager.loadMapFrom(content: layout)
+        let address1 = MapPoint(x: 1, y: 1)
+        let address2 = MapPoint(x: 3, y: 1)
+        let address3 = MapPoint(x: 4, y: 1)
+        
+        dataStore.create(Parking(land: Land(address: address1)))
+        dataStore.create(Parking(land: Land(address: address2), trustLevel: 0.5))
+        dataStore.create(Parking(land: Land(address: address3), trustLevel: 0.5))
+        
+        XCTAssertEqual(calculator.calculateCarsForParking(address: address1), 3)
+        XCTAssertEqual(calculator.calculateCarsForParking(address: address2), 1.5)
+        XCTAssertEqual(calculator.calculateCarsForParking(address: address3), 1.5)
+    }
+    
+    private func makeCalculator() -> ParkingClientCalculator {
+        let dataStore = DataStoreMemoryProvider()
+        let map = GameMap(width: 40, height: 40, scale: 1)
+        let mapManager = GameMapManager(map)
+        return ParkingClientCalculator(mapManager: mapManager, dataStore: dataStore)
+    }
+}
